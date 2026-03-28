b63d74be49da26d931fb395ebffe8207ac7ce7da
samp20096 <samp20096@gmail.com>
2026-03-28 08:08:23 +0300

    Recursive - Finished answers - 09.03.2026

--- 09_03_26/answers.py
+++ 09_03_26/answers.py
@@ -8,7 +8,7 @@ def print_down(n):
 
 print("Answer 1:")
 print_down(5)
-print()
+print("\n")
 
 print("-------------")
 
@@ -30,3 +30,57 @@ print("-------------")
 
 # Answer - 3
 
+def power(base, exp):
+    if exp == 0:
+        return 1
+    else:
+        return base * power(base, exp - 1)
+
+print("Answer 3:")
+print(power(2, 4))
+print()
+
+print("-------------")
+
+# Answer - 4
+
+def max_in_list(lst: list):
+    if len(lst) == 1:
+        return lst[0]
+    else:
+        return max(lst[0], max_in_list(lst[1: ]))
+
+print("Answer 4:")
+print(max_in_list([3, 8, 2, 15, 6]))
+print()
+
+print("-------------")
+
+# Answer - 5
+
+def count_even(lst: list):
+    if len(lst) == 0:
+        return 0
+    else:
+        if lst[0] % 2 == 0:
+            return 1 + count_even(lst[1:])
+        else:
+            return count_even(lst[1: ])
+
+print("Answer 5:")
+print(count_even([2, 5, 8, 7, 6, 3, 10]))
+print()
+
+print("-------------")
+
+# Bonus
+
+def sum_digits(n):
+    if n < 10:
+        return n
+    else:
+        return n % 10 + sum_digits(n // 10)
+
+print("Bonus:")
+print(sum_digits(583))
+print()
